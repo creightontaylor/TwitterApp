2c026ea9246f2a372a015dee878ac29ccd20d2d4
Andrews, John L <John.L.Andrews@disney.com>
2016-08-06 19:12:53 -0700

    add dialog fragment

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/Activity/ComposeFragment.java
@@ -0,0 +1,31 @@
+package com.codepath.apps.mysimpletweets.Activity;
+
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.apps.mysimpletweets.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ComposeFragment extends DialogFragment {
+
+
+    public ComposeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+
+}

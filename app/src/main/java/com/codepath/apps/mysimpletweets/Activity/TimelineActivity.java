147479d7202b07be75477ea881c7a162c67e3896
Andrews, John L <John.L.Andrews@disney.com>
2016-08-13 17:41:39 -0700

    Add setup for viewPager and slidingTabStrip

--- app/src/main/java/com/codepath/apps/mysimpletweets/Activity/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/Activity/TimelineActivity.java
@@ -1,13 +1,19 @@
 package com.codepath.apps.mysimpletweets.Activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.codepath.apps.mysimpletweets.Fragment.ComposeDialogFragment;
-import com.codepath.apps.mysimpletweets.Fragment.TweetsListFragment;
+import com.codepath.apps.mysimpletweets.Fragment.HomeTimelineFragment;
+import com.codepath.apps.mysimpletweets.Fragment.MentionsTimelineFragment;
 import com.codepath.apps.mysimpletweets.Interface.DismissComposeTweetListener;
 import com.codepath.apps.mysimpletweets.Interface.LaunchComposeTweetListener;
 import com.codepath.apps.mysimpletweets.R;
@@ -28,6 +34,8 @@ public class TimelineActivity extends AppCompatActivity implements LaunchCompose
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        setupViewPager();
     }
 
     @Override
@@ -43,6 +51,14 @@ public class TimelineActivity extends AppCompatActivity implements LaunchCompose
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager() {
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+        vpPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager()));
+
+        PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabStrip.setViewPager(vpPager);
     }
 
     private void displayComposeFragment() {
@@ -64,8 +80,8 @@ public class TimelineActivity extends AppCompatActivity implements LaunchCompose
         TwitterApplication.getRestClient().composeTweet(input, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                TweetsListFragment fragmentContainer = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_timeline);
-                fragmentContainer.refreshTweetsTimelineType.refreshTimeLineAndNavigateToTopTweet(fragmentContainer);
+//                TweetsListFragment fragmentContainer = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id .fragment_timeline);
+//                fragmentContainer.refreshTweetsTimelineType.refreshTimeLineAndNavigateToTopTweet(fragmentContainer);
             }
 
             @Override
@@ -73,5 +89,34 @@ public class TimelineActivity extends AppCompatActivity implements LaunchCompose
                 Log.d("DEBUG", "Compose Tweet Failure" + errorResponse.toString());
             }
         });
+    }
+
+    public class TweetsPagerAdapter extends FragmentPagerAdapter {
+        private String tabTitles[] = {"Home", "Mentions"};
+
+        public TweetsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new HomeTimelineFragment();
+            } else if (position == 1){
+                return new MentionsTimelineFragment();
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
     }
 }
